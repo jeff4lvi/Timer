0d46dd6b440dee7617c93568257be60490f27c38
jeff4lvi <jeff_alvi@hotmail.com>
2023-01-03 23:31:50 -0300

    commit to add a dist on git

--- dist/assets/index.63910b4b.js
+++ dist/assets/index.63910b4b.js
@@ -1,6 +1,6 @@
 (function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))r(i);new MutationObserver(i=>{for(const o of i)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&r(a)}).observe(document,{childList:!0,subtree:!0});function n(i){const o={};return i.integrity&&(o.integrity=i.integrity),i.referrerpolicy&&(o.referrerPolicy=i.referrerpolicy),i.crossorigin==="use-credentials"?o.credentials="include":i.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(i){if(i.ep)return;i.ep=!0;const o=n(i);fetch(i.href,o)}})();function dm(e){return e&&e.__esModule&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e}var B={exports:{}},ae={};/**
  * @license React
- * react.production.min.js
+ * react.production.min.js tt
  *
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
